db7d9fc1245560f918bbe0288e5fa9d37fe79103
obende-m <oobendemichael@gmail.com>
2026-06-10 07:15:59 +0100

    chore: trigger Vercel build redeployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+// Trigger Vercel build redeployment
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -13,3 +14,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
